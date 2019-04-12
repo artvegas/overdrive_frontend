4100dc6a291d27049dac4b8c078dd7be29f4d2c8
Jon M <jmat8116@gmail.com>
2019-04-12 03:05:36 -0400

    Fixed /register

--- src/register/register.component.ts
+++ src/register/register.component.ts
@@ -1,16 +1,38 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import {HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions: {headers; observe;} = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'my-auth-token'
+  }),
+  observe: 'response'
+};
 
 @Component({
     selector: 'register',
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent {
     title = 'Create';
 
     async ngAfterViewInit() {
         await this.loadScript('./src/js/main.js');
     }
+
+    constructor (private http: HttpClient) {
+      this.http = http;
+    }
+
+    //Describe FormGroup for user
+    userForm = new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl(''),
+      email: new FormControl('')
+    });
 
     private loadScript(scriptUrl: string) {
         return new Promise((resolve, reject) => {
@@ -19,6 +41,16 @@ export class RegisterComponent {
             scriptElement.onload = resolve;
             document.body.appendChild(scriptElement);
         })
+    }
+
+    onSubmit(user) {
+      console.log(user);
+      console.log("Hit this function");
+      return this.http.post("http://localhost:8080/api/users/register", user, httpOptions)
+        .subscribe( data => {
+            console.log(data);
+        });
+
     }
 
 
