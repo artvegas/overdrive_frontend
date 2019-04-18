47c419de751ddd7e182b8796e2c2785cc0f9fbdf
Jon M <jmat8116@gmail.com>
2019-04-18 16:33:48 -0400

    started working on logout btn and feature

--- src/main-nav/main-nav.component.ts
+++ src/main-nav/main-nav.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MainNavService } from './main-nav.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'main-nav',
@@ -8,5 +10,35 @@ import { Component } from '@angular/core';
 export class MainNavComponent {
     title = 'overdrive-comics';
 
+    constructor(private mainNavService: MainNavService, private router: Router){
+      this.mainNavService = mainNavService;
+      this.router = router;
+    }
 
+    ngOnInit(){
+      this.setButton();
+    }
+    currentLink: string;
+
+    setButton(){
+      let login_btn = document.getElementById("login-btn");
+      //user not logged in
+      if(document.cookie.split["="][1] != ""){
+        login_btn.innerHTML = "Login";
+      } else {
+        login_btn.innerHTML = "Logout";
+      }
+    }
+
+    redirectToPage(){
+      if(document.cookie.split["="][1] != ""){
+        this.router.navigate(["/login"]);
+      } else {
+        this.mainNavService.logoutUser()
+          .subscribe( data => {
+
+          });
+        this.router.navigate(["/login"]);
+      }
+    }
 }
