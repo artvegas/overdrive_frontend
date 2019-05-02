0e67a7bed4f2f8608659192a976bfbeba78ec1fa
Jon M <jmat8116@gmail.com>
2019-05-01 20:20:31 -0400

    Added reader service

--- src/reader/reader.component.ts
+++ src/reader/reader.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ReaderService } from './reader.service';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     selector: 'reader',
@@ -8,6 +10,18 @@ import { Component } from '@angular/core';
 export class ReaderComponent {
     title = 'Genre';
 
+    constructor(private readerService: ReaderService, private route: ActivatedRoute){
+      this.readerService = readerService;
+      this.route = route;
+    }
 
-
+    callLikeChapter(element){
+      let currentId = this.route.snapshot.paramMap.get("id");
+      this.readerService.likeChapter(currentId)
+        .subscribe( data=> {
+          console.log("inside callLikeChapter");
+          console.log(data);
+        });
+      element.style.cssText = "color: red;"
+    }
 }
